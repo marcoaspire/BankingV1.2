250b803f4e0e52546107a0314a4cd9ae64b2d3c7
Marco Antonio <marco.antonio@aspiresys.com>
2022-02-27 14:02:02 -0600

    fix: delete comments

--- Program.cs
+++ Program.cs
@@ -19,16 +19,6 @@ namespace BankingV1._8
             //FileBO.UsersLoad();
             bank.LoginMenu();
 
-            /*
-                AccountDataAccess dataAccess = new AccountDataAccess();
-                DataSet ds =dataAccess.GetAllAccountsFromDB();
-                foreach (DataRow item in ds.Tables[0].Rows)
-                {
-                    Console.WriteLine(item["AccountNumber"].ToString());
-                    //Department dept = new Department(Convert.ToInt32(item["deptno"].ToString()), item["dname"].ToString(), item["loc"].ToString());
-                    //list.Add(dept);
-                }
-            */
         }
     }
 }
